dfa0928bcd475d0be0cddf24a6063123583030b4
tossapon <tnuanchuay@gmail.com>
2023-05-27 20:42:47 +0700

    finish buyTowerScene

--- src/game/towers.ts
+++ src/game/towers.ts
@@ -21,6 +21,73 @@ export const TowerModels: TowerData[] = [
     },
     // Medieval
     {
+        type: "Catapult",
+        sprite: 'tower',
+        attackDamage: 10,
+        attackSpeed: 1500,
+        attackRange: 150,
+        cost: 300,
+        attackObjectSprite: 'arrow'
+    },
+    {
+        type: "Medieval",
+        sprite: 'tower',
+        attackDamage: 10,
+        attackSpeed: 1000,
+        attackRange: 150,
+        cost: 300,
+        attackObjectSprite: 'arrow'
+    },
+    // Ancient
+    {
+        type: "AncientTower",
+        sprite: 'tower',
+        attackDamage: 1,
+        attackSpeed: 800,
+        attackRange: 100,
+        cost: 100,
+        attackObjectSprite: 'arrow'
+    },
+    // Medieval
+    {
+        type: "Catapult",
+        sprite: 'tower',
+        attackDamage: 10,
+        attackSpeed: 1500,
+        attackRange: 150,
+        cost: 300,
+        attackObjectSprite: 'arrow'
+    },
+    {
+        type: "Medieval",
+        sprite: 'tower',
+        attackDamage: 10,
+        attackSpeed: 1000,
+        attackRange: 150,
+        cost: 300,
+        attackObjectSprite: 'arrow'
+    },
+    // Ancient
+    {
+        type: "AncientTower",
+        sprite: 'tower',
+        attackDamage: 1,
+        attackSpeed: 800,
+        attackRange: 100,
+        cost: 100,
+        attackObjectSprite: 'arrow'
+    },
+    // Medieval
+    {
+        type: "Catapult",
+        sprite: 'tower',
+        attackDamage: 10,
+        attackSpeed: 1500,
+        attackRange: 150,
+        cost: 300,
+        attackObjectSprite: 'arrow'
+    },
+    {
         type: "Medieval",
         sprite: 'tower',
         attackDamage: 10,
--- src/scenes/buyTower.ts
+++ src/scenes/buyTower.ts
@@ -6,13 +6,13 @@ import {Slot} from "../objects/slot";
 import {Label, FixWidthSizer} from "phaser3-rex-plugins/templates/ui/ui-components";
 import {DataKey} from "../constants/gameData";
 
-const ColorLabel = 0x999999;
-const ColorSlider = 0x222222;
-const ColorDark = 0x000000;
+const colorLabel = 0x999999;
+const colorSlider = 0x222222;
+const colorDark = 0x000000;
 
-const KeyTowerDataModel = "tower_model"
+const keyTowerDataModel = "tower_model"
 
-const ConfigGrid: FixWidthSizer.IConfig = {
+const configGrid: FixWidthSizer.IConfig = {
     space: {
         left: 3,
         right: 3,
@@ -38,16 +38,16 @@ export class BuyingTowerScene extends Scene {
     }
 
     preload = () => {
-
+        this.load.spritesheet('towerBg', 'assets/towerBg.png', {frameWidth: 60, frameHeight: 60});
     }
 
     create = () => {
         this.game.scene.pause(SceneName.BattleScene);
         const {width, height} = this.game.canvas;
-        this.add.rectangle(0, 0, width, height, 0x000000, 0.1)
+        this.add.rectangle(0, 0, width, height, 0x000000, 0)
             .setOrigin(0, 0)
             .setInteractive()
-            .on('pointerup', () => {
+            .on('pointerdown', () => {
                 this.resumeGame();
             });
 
@@ -59,7 +59,7 @@ export class BuyingTowerScene extends Scene {
 
             scrollMode: 0,
 
-            background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 10, ColorDark),
+            background: this.add.rectangle(0, 0, 2, 2, colorDark).setInteractive(),
 
             panel: {
                 child: this.createGrid(),
@@ -69,8 +69,8 @@ export class BuyingTowerScene extends Scene {
             },
 
             slider: {
-                track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, ColorSlider),
-                thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, ColorLabel),
+                track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, colorSlider),
+                thumb: this.rexUI.add.roundRectangle(0, 0, 0, 0, 13, colorLabel),
             },
 
             mouseWheelScroller: {
@@ -80,18 +80,16 @@ export class BuyingTowerScene extends Scene {
 
             header: this.rexUI.add.label({
                 height: 30,
-
                 orientation: 0,
                 text: this.add.text(0, 0, ['Construct a defend tower !']),
                 align: 'center'
-            }),
+            }).setInteractive(),
 
             space: {
                 left: 10,
                 right: 10,
                 top: 10,
                 bottom: 10,
-
                 panel: 10,
                 header: 10,
                 footer: 10,
@@ -102,8 +100,9 @@ export class BuyingTowerScene extends Scene {
         scrollablePanel
             .setChildrenInteractive({})
             .on('child.click', (child, pointer, event) => {
+                debugger
                 const label = child as Label;
-                const data = label.data.get(KeyTowerDataModel) as TowerData;
+                const data = label.data.get(keyTowerDataModel) as TowerData;
                 this.slot.setTower(data);
                 this.scene.get(SceneName.BattleScene).data.inc(DataKey.Money, data.cost * -1);
                 this.resumeGame();
@@ -111,29 +110,33 @@ export class BuyingTowerScene extends Scene {
     }
 
     createGrid = () => {
-        // Create table body
-        const sizer = this.rexUI.add.fixWidthSizer(ConfigGrid)
-            .addBackground(this.rexUI.add.roundRectangle(0, 0, 10, 10, 0, ColorDark));
+        const sizer = this.rexUI.add.fixWidthSizer(configGrid)
+            .addBackground(this.rexUI.add.roundRectangle(0, 0, 10, 10, 0, colorDark));
 
         TowerModels
             .map(model =>
-                this.rexUI.add.label(this.towerLabelConfig(model))
-                    .setData(KeyTowerDataModel, model)
+                this.rexUI.add.label(this.getTowerLabelConfig(model))
+                    .setData(keyTowerDataModel, model)
             )
             .forEach(label => sizer.add(label));
 
         return sizer;
     }
 
-    towerLabelConfig = (model: TowerData): Label.IConfig => {
+    getTowerLabelConfig = (model: TowerData): Label.IConfig => {
+        const container = this.add.container()
+            .add(this.add.image(0, -30, model.sprite).setOrigin(0.5, 0.5))
+            .add(this.add.text(0, 30, [`$${model.cost}`, `ATK: ${model.attackDamage}`, `RANGE: ${model.attackRange}`, `SPEED: ${model.attackSpeed / 1000}s`], {
+                fontSize: 18,
+                align: 'center'
+            }).setOrigin(0.5, 0.5));
+
         return {
             width: 150,
             height: 150,
 
-            background: this.rexUI.add.roundRectangle(0, 0, 0, 0, 14, ColorLabel),
-            text: this.add.text(0, 0, [`$${model.cost}`, `ATK: ${model.attackDamage}`, `RANGE: ${model.attackRange}`, `SPEED: ${model.attackSpeed / 1000}s`], {
-                fontSize: 18
-            }),
+            background: this.add.image(0, 0, 'towerBg').setScale(1.2, 1.2),
+            text: container,
 
             align: 'center',
             space: {
